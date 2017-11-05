50d6396acd4db6bb6b40be43b2f825b21daa3f07
Bryan Brito <bhyanbrito+github@gmail.com>
2017-11-05 06:57:16 -0200

    Teste os argv de entrada do catarata.c

--- test/cunit_main.c
+++ test/cunit_main.c
@@ -1,15 +1,16 @@
+#include <stdlib.h>
 #include "CUnit/Basic.h"
 #include "Imagem.h"
 
 
-void teste_imagem_ppm();
+void teste_input();
 
 int main(){
     CU_initialize_registry();
 
     CU_pSuite teste = CU_add_suite("Suite de teste", NULL, NULL);
 
-    CU_add_test(teste, "teste_imagem_ppm", teste_imagem_ppm);
+    CU_add_test(teste, "teste_input", teste_input);
 
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
@@ -17,4 +18,9 @@ int main(){
     CU_cleanup_registry();
 
     return CU_get_error();
+}
+
+void teste_input(){
+    //Teste de passagem de paramentros para o o main do programa.
+    system("../bin/main -i imagem.ppm -f ppm -o diagnostico.txt");
 }
