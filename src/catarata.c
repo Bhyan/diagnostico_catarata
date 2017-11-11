3173feccecfd40ba8ea6d6ed4c3cee9a6b789ba0
Bryan Brito <bhyanbrito+github@gmail.com>
2017-11-11 02:38:50 -0200

    Mudança do nome do arquivo de main.c para catarata.c

--- src/catarata.c
+++ src/catarata.c
@@ -6,14 +6,15 @@
 
 int main(int argc, char **argv) {
     int paramentros;
-    char nome[25], formato[5], saida[25];
+    char nome[25], formato[4], saida[25];
 
-    if(argc == 1){ //Caso o usuario rode o programa sem nenhum paramentro sera retornado o help.
+    if(argc == 1){ //Caso o usuário rode o programa sem nenhum parâmetro sera retornado o help.
         printf("-i   <input-image>\n"
                 "-f   <input-image-format>\n"
                 "-o   <diagnose-file>\n"
                 "-h   <help>\n");
     }
+
     else{
         /*Função da biblioteca unistd. Usada para casar os paramentos -i, -f,
          * -o e o adicional -h para help, assim que casado ele pega o argumento
@@ -21,12 +22,12 @@ int main(int argc, char **argv) {
          *  formato do argumento para leitura, e entrada padrão.
          */
         while((paramentros = getopt(argc, argv, "i:f:o:h")) != -1){
-            switch(paramentros) {
+            switch(paramentros){
                 case 'i':
                     snprintf(nome, 25, "%s", optarg); 
                     break;
                 case 'f':
-                    snprintf(formato, 5, "%s", optarg); 
+                    snprintf(formato, 4, "%s", optarg); 
                     break;
                 case 'o':
                     snprintf(saida, 25, "%s", optarg); 
@@ -41,7 +42,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    if(strcmp(formato, "ppm") == 0){
+    if(strcmp(formato, "ppm") != 0){
         imagem_ppm(nome);
     }
 
